cc905ba3547fff6805756309037693e1b86be02e
Ian C. Anderson <ian@iancanderson.com>
2022-11-02 19:22:29 -0400

    Use donburi for ECS

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"log"
+	"math/rand"
+	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/yohamta/donburi"
+	"github.com/yohamta/donburi/ecs"
 )
 
-type Game struct{}
+type Game struct {
+	ecs *ecs.ECS
+}
 
 func (g *Game) Update() error {
 	return nil
@@ -21,10 +27,32 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 	return 320, 240
 }
 
+func NewGame() *Game {
+	return &Game{
+		ecs: createECS(),
+	}
+}
+
+func createECS() *ecs.ECS {
+	world := createWorld()
+	ecs := ecs.NewECS(world)
+	return ecs
+}
+
+func createWorld() donburi.World {
+	world := donburi.NewWorld()
+	return world
+}
+
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	ebiten.SetWindowTitle("Gandermerge")
+
+	ebiten.SetWindowSize(800, 600)
+	ebiten.SetWindowSizeLimits(300, 200, -1, -1)
+	ebiten.SetFPSMode(ebiten.FPSModeVsyncOffMaximum)
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+	rand.Seed(time.Now().UTC().UnixNano())
+	if err := ebiten.RunGame(NewGame()); err != nil {
 		log.Fatal(err)
 	}
 }
