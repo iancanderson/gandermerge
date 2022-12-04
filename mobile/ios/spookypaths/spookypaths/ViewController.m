427b9885300415022e2ea07a3a8c6067d5c01e72
Ian C. Anderson <ian@iancanderson.com>
2022-12-03 21:21:56 -0500

    Configure admob banner ad at bottom of screen

--- mobile/ios/spookypaths/spookypaths/ViewController.m
+++ mobile/ios/spookypaths/spookypaths/ViewController.m
@@ -6,8 +6,11 @@
 //
 
 #import "ViewController.h"
+@import GoogleMobileAds;
 
 @interface ViewController ()
+
+@property(nonatomic, strong) GADBannerView *bannerView;
 
 @end
 
@@ -16,7 +19,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    // In this case, we instantiate the banner with desired ad size.
+    self.bannerView = [[GADBannerView alloc]
+        initWithAdSize:GADAdSizeBanner];
+    
+    [self addBannerViewToView:self.bannerView];
+
+    self.bannerView.adUnitID = @"ca-app-pub-3940256099942544/2934735716";
+    self.bannerView.rootViewController = self;
+    
+    [self.bannerView loadRequest:[GADRequest request]];
 }
 
+- (void)addBannerViewToView:(UIView *)bannerView {
+  bannerView.translatesAutoresizingMaskIntoConstraints = NO;
+  [self.view addSubview:bannerView];
+  [self.view addConstraints:@[
+    [NSLayoutConstraint constraintWithItem:bannerView
+                               attribute:NSLayoutAttributeBottom
+                               relatedBy:NSLayoutRelationEqual
+                                  toItem:self.bottomLayoutGuide
+                               attribute:NSLayoutAttributeTop
+                              multiplier:1
+                                constant:0],
+    [NSLayoutConstraint constraintWithItem:bannerView
+                               attribute:NSLayoutAttributeCenterX
+                               relatedBy:NSLayoutRelationEqual
+                                  toItem:self.view
+                               attribute:NSLayoutAttributeCenterX
+                              multiplier:1
+                                constant:0]
+                                ]];
+}
 
 @end
