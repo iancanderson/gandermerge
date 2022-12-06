d9f6edbf22980170c8b63a6c2a8cba8833090ebb
Ian C. Anderson <ian@iancanderson.com>
2022-12-05 20:35:25 -0500

    Try out a "rewarded ad"
    TODO: how to call back into the Go code to activate the reward??

--- mobile/ios/spookypaths/spookypaths/ViewController.m
+++ mobile/ios/spookypaths/spookypaths/ViewController.m
@@ -8,13 +8,54 @@
 #import "ViewController.h"
 @import GoogleMobileAds;
 
-@interface ViewController ()
+@interface ViewController ()<GADFullScreenContentDelegate>
 
 @property(nonatomic, strong) GADBannerView *bannerView;
+@property(nonatomic, strong) GADRewardedAd *rewardedAd;
 
 @end
 
 @implementation ViewController
+
+- (void)loadRewardedAd {
+  GADRequest *request = [GADRequest request];
+  [GADRewardedAd
+       loadWithAdUnitID:@"ca-app-pub-3940256099942544/1712485313"
+                request:request
+      completionHandler:^(GADRewardedAd *ad, NSError *error) {
+        if (error) {
+          NSLog(@"Rewarded ad failed to load with error: %@", [error localizedDescription]);
+          return;
+        }
+        self.rewardedAd = ad;
+        NSLog(@"Rewarded ad loaded.");
+        self.rewardedAd.fullScreenContentDelegate = self;
+      
+        [self.rewardedAd presentFromRootViewController:self
+                                    userDidEarnRewardHandler:^{
+                                    GADAdReward *reward =
+                                        self.rewardedAd.adReward;
+                                    // TODO: Reward the user!
+                                  }];
+      
+  }];
+}
+
+/// Tells the delegate that the ad failed to present full screen content.
+- (void)ad:(nonnull id<GADFullScreenPresentingAd>)ad
+    didFailToPresentFullScreenContentWithError:(nonnull NSError *)error {
+    NSLog(@"Ad did fail to present full screen content.");
+}
+
+/// Tells the delegate that the ad will present full screen content.
+- (void)adWillPresentFullScreenContent:(nonnull id<GADFullScreenPresentingAd>)ad {
+    NSLog(@"Ad will present full screen content.");
+}
+
+/// Tells the delegate that the ad dismissed full screen content.
+- (void)adDidDismissFullScreenContent:(nonnull id<GADFullScreenPresentingAd>)ad {
+    NSLog(@"Ad did dismiss full screen content.");
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -30,6 +71,8 @@
     self.bannerView.rootViewController = self;
     
     [self.bannerView loadRequest:[GADRequest request]];
+
+    [self loadRewardedAd];
 }
 
 - (void)addBannerViewToView:(UIView *)bannerView {
